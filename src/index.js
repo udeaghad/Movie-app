43e8be2a61de8af6bb325571aa30ce79d21d01a7
udeaghad <dozie.udeagha@gmail.com>
2022-06-28 14:43:50 +0100

    Choose API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import renderUI from './modules/renders.js';
-import getList from './modules/createList.js'
+import getList from './modules/createList.js';
 
 renderUI();
 getList();
